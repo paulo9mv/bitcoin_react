c7249682390e4f5514fc9e78363e651a20383678
Paulo Viana <paulo9mv@yahoo.com.br>
2020-07-17 15:20:31 -0400

    Consulta API funcionando

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+import styles from '../features/counter/Counter.module.css';
+
+export function MainPage() {
+
+
+
+    const [moeda, setMoeda] = useState("");
+    const [value, setValue] = useState("");
+
+
+
+    const handleClick = (moedaSigla: number) => {
+
+        let moedaNome;
+
+        switch (moedaSigla) {
+            case 0: {
+                moedaNome = "USD";
+                setMoeda(moedaNome);                
+                break;
+            }
+            case 1: {
+                moedaNome = "GBP";
+                setMoeda(moedaNome);
+                break;
+            }
+            case 2:{
+                moedaNome = "EUR";
+                setMoeda(moedaNome);
+                break;
+            }
+            default:{
+                console.log("Entrada inválida");
+            }
+        }
+
+        fetch('https://api.coindesk.com/v1/bpi/currentprice.json').
+        then(res => res.json()).then(
+            res => setValue(res.bpi[moedaNome].rate)
+        )
+    }
+
+
+    return (
+        <div>
+            Preço do Bitcoin
+            <div className={styles.row}>
+                <button
+                    className={styles.button}
+                    aria-label="USD"
+                    onClick={() => handleClick(0)}
+                >
+                    USD
+                </button>
+                <button
+                    className={styles.button}
+                    aria-label="GBP"
+                    onClick={() => handleClick(1)}
+                >
+                    GBP
+                </button>
+                <button
+                    className={styles.button}
+                    aria-label="EUR"
+                    onClick={() => handleClick(2)}
+                >
+                    EUR
+                </button>
+            </div>
+        {value} {moeda}
+            <div>
+                {/**Inserir gráfico aqui */}
+                </div>
+        </div>
+    );
+}
