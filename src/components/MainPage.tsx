b1a16fc4f3c90fb803d37957a124b82a42da4d2b
Paulo Viana <paulo9mv@yahoo.com.br>
2020-07-20 15:56:46 -0400

    Adiciona loading ao inicializar a pagina

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -5,16 +5,19 @@ import {
   fetchGraphApi,
   selectGraphValues,
   selectError,
+  selectData,
 } from "../store/reducer";
 import { formatDate } from "../helpers/utils";
 import MyChart from "./Chart";
 import { SelectionBar } from "./SelectionBar";
 import { Content } from "./Content";
 import { ErrorMessage } from "./ErrorMessage";
+import ReactLoading from 'react-loading';
 
 export function MainPage() {
   const dispatch = useDispatch();
   const error = useSelector(selectError);
+  const { lastUpdate } = useSelector(selectData);
   const graphValues: Array<number> = useSelector(selectGraphValues);
 
   useEffect(() => {
@@ -32,10 +35,15 @@ export function MainPage() {
 
   return (
     <div>
-      <SelectionBar />
-      <Content />
-      {graphValues.length > 0 && <MyChart values={graphValues} />}
-      {error && <ErrorMessage />}
+      {lastUpdate === 0 ?
+      <ReactLoading type={'spinningBubbles'} color={'rgb(255,200,0)'} height={50} width={375} />
+      : <div>
+          <SelectionBar />
+          <Content />
+          {graphValues.length > 0 && <MyChart values={graphValues} />}
+          {error && <ErrorMessage />}
+        </div>
+      }  
     </div>
   );
 }
